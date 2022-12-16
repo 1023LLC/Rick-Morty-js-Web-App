e98d254559ed1008212db9bf67c606113ec3d44d
SusanGithaiga <susan.githaiga@student.moringaschool.com>
2022-12-16 04:14:37 +0300

    My Commit

--- index.js
+++ index.js
@@ -1,33 +1,36 @@
-var requestOptions = {
-    method: 'GET',
-    redirect: 'follow'
-  };
-//   Get button, add an event listener and create a function.
-  document.getElementById("view").addEventListener('click', getData);
+// var requestOptions = {
+//     method: 'GET',
+//     redirect: 'follow'
+//   };
+// //   Get button, add an event listener and create a function.
+//   document.getElementById("view").addEventListener('click', getData);
 
-  function getData(){
-    // Get API.
-  fetch("https://www.reddit.com/r/Wallstreetbets/top.json?limit=10&t=year")
-    .then(response => response.json())
-    .then(data => {
+//   function getData(){
+//     // Get API.
+//   fetch("https://www.reddit.com/r/Wallstreetbets/top.json?limit=10&t=year")
+//     .then(response => response.json())
+//     .then(data => {
 
-    let authors = data.posts;
-    // console.log(authors);
-    // Get data value
-    let result ="<p><center>Recent popular posts on Reddit</center></p>";
+//     let authors = data.posts;
+//     // console.log(authors);
+//     // Get data value
+//     let result ="<p><center>Recent popular posts on Reddit</center></p>";
 
-    // Get data loop through.
-    authors.forEach(function(lists){
-        result += `
-       <p class="lead">Author: ${lists.author} </p>
-       <p class="lead">Title: ${lists.title} </p>
-       <p class="lead">Kind: ${lists.kind} </p>
-       <p class="lead">Thread: ${lists.text} </p>
-        <a id ="refresh" class="btn btn-primary btn-lg" href="#" role="button" style="background-color: orangered; color: white; border-color: white;">Refresh</a>
-        `;
-    });
+//     // Get data loop through.
+//     authors.forEach(function(lists){
+//         result += `
+//        <p class="lead">Author: ${lists.author} </p>
+//        <p class="lead">Title: ${lists.title} </p>
+//        <p class="lead">Kind: ${lists.kind} </p>
+//        <p class="lead">Thread: ${lists.text} </p>
+//         <a id ="refresh" class="btn btn-primary btn-lg" href="#" role="button" style="background-color: orangered; color: white; border-color: white;">Refresh</a>
+//         `;
+//     });
 
-    // Show data on the screen.
-    document.getElementsByClassName('lead').innerHTML = result;
-  });
-};
+//     // Show data on the screen.
+//     document.getElementsByClassName('lead').innerHTML = result;
+//   });
+// };
+
+
+
