c4cc09606818fd352164146f20dcda62a48306f0
SusanGithaigaN <117893498+SusanGithaigaN@users.noreply.github.com>
2022-12-19 10:09:59 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,33 +1,24 @@
-var requestOptions = {
-    method: 'GET',
-    redirect: 'follow'
-  };
-//   Get button, add an event listener and create a function.
-  document.getElementById("view").addEventListener('click', getData);
+// Fetch the data from the public API URL.
+fetch("https://rickandmortyapi.com/api/character")
 
-  function getData(){
-    // Get API.
-  fetch("https://www.reddit.com/r/Wallstreetbets/top.json?limit=10&t=year")
+    // Jsonify the response.
     .then(response => response.json())
-    .then(data => {
+    .then(data =>makeCards(data.results))
+makeCards.forEach(character)
+    function makeCards(charactersArray){
+        // console.log(morty)
+        // Create a variable that references our characters.
+        const cardContainer = document.querySelector('.lead')
+        console.log(charactersArray)
+        // Iterate the characters array to create a new card for each character.
+        charactersArray.forEach(character => {
+            cardContainer.innerHTML = cardContainer.innerHTML +
+             `
+             <h2>${character.name}</<h2>
+            <img src =${character.image}></img>
+            
+            `
+        })   
+        // console.log(charactersArray)        
+}
 
-    let authors = data.posts;
-    console.log(authors);
-    // Get data value
-    let result ="<p><center>Recent popular posts on Reddit</center></p>";
-
-    // Get data loop through.
-    authors.forEach(function(lists){
-        result += `
-       <p class="lead">Author: ${lists.author} </p>
-       <p class="lead">Title: ${lists.title} </p>
-       <p class="lead">Kind: ${lists.kind} </p>
-       <p class="lead">Thread: ${lists.text} </p>
-        <a id ="refresh" class="btn btn-primary btn-lg" href="#" role="button" style="background-color: orangered; color: white; border-color: white;">Refresh</a>
-        `;
-    });
-
-    // Show data on the screen.
-    document.getElementsByClassName('lead').innerHTML = result;
-  });
-};
