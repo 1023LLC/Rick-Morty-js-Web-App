37cdafac67c3654681d827543333e1ef6c585669
SusanGithaiga <susan.githaiga@student.moringaschool.com>
2022-12-26 07:33:51 +0300

    My commit

--- index.js
+++ index.js
@@ -35,36 +35,91 @@ makeCards.forEach(character)
 }
 
 
+// // Create a variable that references our characters.
+// let displayCharacters = document.querySelector('#search-button');
+// // Reference the data from our API to a variable.
+// let characterContainer = document.querySelector('#search-deck');
+// makeNewCards.forEach(searchCharacter)
+//     function makeNewCards(searchCharactersArray){
+//         // Add an event listener to the search button
+//         displayCharacters.addEventListener('click', () => {       
+//         // Iterate the characters array to create a new card for each character.
+//         searchCharactersArray.forEach(searchCharacter => {
+//             characterContainer.innerHTML = characterContainer.innerHTML +
+//             `
+//                 <div class="card-deck">
+//                     <div class="card">
+//                         <img class="card-img-top"alt="Card image cap" src =${character.image}></img>
+//                             <div class="card-body">
+//                                 <h5 class="card-title">Name: ${character.name}</<h5>
+//                                 <p class="card-text"  style="font-size-10px;">Gender: ${character.gender}</<p>
+                                
+//                             </div>
+//                             <div class="card-footer">
+//                                 <small class="text-muted">Planet: ${character.origin.name}</small>
+//                             </div>
+//                     </div>
+//                 </div>        
+//             `
+//         }); 
+//     }); 
+// }
+
+
+
 // Create a search event.
 const form =  document.querySelector('#search-btn');
-const inputName = document.querySelector('#search');
-const characterName =  document.createElement('div');
-const characterList = document.createElement('div');
+const inputCharacter = document.querySelector('.form-control mr-sm-2');
+const characterList = document.getElementById('search-deck');
+const characterName = document.querySelectorAll('#character-name');
 
-const searchCharacter = () => {
-    characterList.forEach((characterName) => {
-
-    })
-}
-
-const searchCharacterName = () => {
-    form.addEventListener('submit', (e) => {
-        e.preventDefault();
-        fetch(`https://rickandmortyapi.com/api/character ${inputName.value}`,{
-    })
-    .then(response=>response.json())
-    .then(data=>{
-        data.items.forEach((element)=>{
-            const myCharacter= document.createElement('div');
-            myCharacter.innerHTML = `<div class="card">${element.login}</li> <img src= "${element.avatar_url}"/>  <a href="${element.repos_url}">Repository Link</a>`
-            characterName.appendChild(myCharacter);
-        })    
-    })
+const searchCharacterName= () => {
+    characterName.forEach((characterName)=> {
+        characterName.addEventListener('click', (event)=>{
+            fetch(`https://rickandmortyapi.com/api/character`)
+        })
+    .then((response)=> response.json())
+    .then((data)=> data.forEach((element)=>{
+        const li = document.createElement('li');
+        li.textContent = element.name;
+        console.log(element.name);
+        characterList.appendChild(li);
+    }))
 })
-};
+}
+    
+const searchCharacter = ()=>{
+    form.addEventListener('submit', (e)=>{
+        e.preventDefault();
+        fetch("https://rickandmortyapi.com/api/character")
+        .then(response=>response.json())
+        .then(data=>{ 
+            data.items.forEach((element)=>{
+                const character = document.createElement('div');
+                character.innerHTML =
+                `
+                    <div class="card">
+                            <img class="card-img-top"alt="Card image cap" src =${character.image}></img>
+                                <div class="card-body">
+                                    <h5 class="card-title">Name: ${character.name}</<h5>
+                                    <p class="card-text"  style="font-size-10px;">Gender: ${character.gender}</<p>
+                                    
+                                </div>
+                                <div class="card-footer">
+                                    <small class="text-muted">Planet: ${character.origin.name}</small>
+                                </div>
+                        </div>
+                `
+                characterList.appendChild(character);
+            })
+        
+        })
 
-searchCharacterName();
+    })
+};
 searchCharacter();
+searchCharacterName();
+
 
 // CAROUSEL!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 {/* <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
