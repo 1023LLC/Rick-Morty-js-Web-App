828ddb11448161c4e4398b2bf61d5fc72c1fb879
SusanGithaiga <susan.githaiga@student.moringaschool.com>
2022-12-26 08:57:23 +0300

    My commit

--- index.js
+++ index.js
@@ -21,7 +21,10 @@ makeCards.forEach(character)
                         <img class="card-img-top"alt="Card image cap" src =${character.image}></img>
                             <div class="card-body">
                                 <h5 class="card-title">Name: ${character.name}</<h5>
-                                <p class="card-text"  style="font-size-10px;">Gender: ${character.gender}</<p>
+                                <p class="card-text"  style="font-size-10px;">Gender: ${character.gender}</p>
+                                <br>
+                                <p class="card-text"  style="font-size-10px;">Status: ${character.status}</p>
+                                <p class="card-text"  style="font-size-10px;">Species: ${character.species}</p>
                                 
                             </div>
                             <div class="card-footer">
@@ -34,38 +37,9 @@ makeCards.forEach(character)
     }); 
 }
 
-
-// // Create a variable that references our characters.
-// let displayCharacters = document.querySelector('#search-button');
-// // Reference the data from our API to a variable.
-// let characterContainer = document.querySelector('#search-deck');
-// makeNewCards.forEach(searchCharacter)
-//     function makeNewCards(searchCharactersArray){
-//         // Add an event listener to the search button
-//         displayCharacters.addEventListener('click', () => {       
-//         // Iterate the characters array to create a new card for each character.
-//         searchCharactersArray.forEach(searchCharacter => {
-//             characterContainer.innerHTML = characterContainer.innerHTML +
-//             `
-//                 <div class="card-deck">
-//                     <div class="card">
-//                         <img class="card-img-top"alt="Card image cap" src =${character.image}></img>
-//                             <div class="card-body">
-//                                 <h5 class="card-title">Name: ${character.name}</<h5>
-//                                 <p class="card-text"  style="font-size-10px;">Gender: ${character.gender}</<p>
-                                
-//                             </div>
-//                             <div class="card-footer">
-//                                 <small class="text-muted">Planet: ${character.origin.name}</small>
-//                             </div>
-//                     </div>
-//                 </div>        
-//             `
-//         }); 
-//     }); 
-// }
-
-
+// Change view button  color onClick().
+let myBtn = document.querySelector('#view');
+myBtn.addEventListener('click', () => myBtn.style.color = '##ccffcc')
 
 // Create a search event.
 const form =  document.querySelector('#search-btn');
@@ -75,23 +49,13 @@ const characterName = document.querySelectorAll('#character-name');
 
 const searchCharacterName= () => {
     characterName.forEach((characterName)=> {
-        characterName.addEventListener('click', (event)=>{
-            fetch(`https://rickandmortyapi.com/api/character`)
-        })
-    .then((response)=> response.json())
-    .then((data)=> data.forEach((element)=>{
-        const li = document.createElement('li');
-        li.textContent = element.name;
-        console.log(element.name);
-        characterList.appendChild(li);
-    }))
-})
+   })
 }
     
 const searchCharacter = ()=>{
     form.addEventListener('submit', (e)=>{
         e.preventDefault();
-        fetch("https://rickandmortyapi.com/api/character")
+        fetch(`https://rickandmortyapi.com/api/character?name=${characterName.name}`)
         .then(response=>response.json())
         .then(data=>{ 
             data.items.forEach((element)=>{
@@ -119,34 +83,3 @@ const searchCharacter = ()=>{
 };
 searchCharacter();
 searchCharacterName();
-
-
-// CAROUSEL!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-{/* <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-<ol class="carousel-indicators">
-    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-</ol>
-<div class="carousel-inner">
-    
-        <div class="carousel-item active">
-                <img class="d-block w-100" alt="Card image cap" src =${character.image} alt="First slide"></img>
-                <div class="carousel-caption d-none d-md-block">   
-                    <h5>Name: ${character.name}</<h5>
-                    <p>Gender: ${character.gender}</<p>
-                    <p>Planet: ${character.origin.name}</p>
-                    <p>Episode: ${character.episode}</p>
-                </div>
-        </div>
-    
-    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="sr-only">Previous</span>
-    </a>
-    <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="sr-only">Next</span>
-    </a>
-</div> 
-</div>  */}
